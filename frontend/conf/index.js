3d43131b0986c0cddf8d218feffaf21bd8b00525
Sangamesh <sangameshbiradar586141@gmail.com>
2023-01-24 21:39:34 +0530

    Updated backedn API to Cyclic

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,3 +1,7 @@
-const config = { backendEndpoint: "https://dreamdestination-api.onrender.com" };
+// Render Backend API
+// const config = { backendEndpoint: "https://dreamdestination-api.onrender.com" };
+
+// Cyclic Backend API
+const config = { backendEndpoint: "https://erin-graceful-vulture.cyclic.app" };
 
 export default config;
